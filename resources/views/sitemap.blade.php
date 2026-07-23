edbd715c4ee3f17a53775496d521e373a4f2c1c8
Ahmet <ahmet@dioreal.com>
2026-07-23 14:40:39 +0300

    Update sitemap.xml with xhtml hreflang links for TR/EN alternate URL mapping and verify robots.txt

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -1,107 +1,140 @@
 <?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+        xmlns:xhtml="http://www.w3.org/1999/xhtml">
     
     <!-- Static Pages -->
+    @php
+        $staticRoutes = [
+            'home' => ['freq' => 'daily', 'prio' => '1.0'],
+            'hakkimizda' => ['freq' => 'weekly', 'prio' => '0.8'],
+            'oteller' => ['freq' => 'daily', 'prio' => '0.9'],
+            'yatlar' => ['freq' => 'daily', 'prio' => '0.9'],
+            'restoranlar' => ['freq' => 'daily', 'prio' => '0.9'],
+            'gezi-rehberi' => ['freq' => 'daily', 'prio' => '0.9'],
+            'etkinlikler' => ['freq' => 'daily', 'prio' => '0.9'],
+            'journal' => ['freq' => 'daily', 'prio' => '0.9'],
+        ];
+    @endphp
+
+    @foreach($staticRoutes as $routeName => $meta)
     <url>
-        <loc>{{ route('home') }}</loc>
-        <changefreq>daily</changefreq>
-        <priority>1.0</priority>
+        <loc>{{ route($routeName) }}</loc>
+        <xhtml:link rel="alternate" hreflang="tr" href="{{ route($routeName, ['lang' => 'tr']) }}"/>
+        <xhtml:link rel="alternate" hreflang="en" href="{{ route($routeName, ['lang' => 'en']) }}"/>
+        <xhtml:link rel="alternate" hreflang="x-default" href="{{ route($routeName) }}"/>
+        <changefreq>{{ $meta['freq'] }}</changefreq>
+        <priority>{{ $meta['prio'] }}</priority>
     </url>
-    <url>
-        <loc>{{ route('hakkimizda') }}</loc>
-        <changefreq>weekly</changefreq>
-        <priority>0.8</priority>
-    </url>
-    <url>
-        <loc>{{ route('oteller') }}</loc>
-        <changefreq>daily</changefreq>
-        <priority>0.9</priority>
-    </url>
-    <url>
-        <loc>{{ route('yatlar') }}</loc>
-        <changefreq>daily</changefreq>
-        <priority>0.9</priority>
-    </url>
-    <url>
-        <loc>{{ route('restoranlar') }}</loc>
-        <changefreq>daily</changefreq>
-        <priority>0.9</priority>
-    </url>
-    <url>
-        <loc>{{ route('gezi-rehberi') }}</loc>
-        <changefreq>daily</changefreq>
-        <priority>0.9</priority>
-    </url>
-    <url>
-        <loc>{{ route('etkinlikler') }}</loc>
-        <changefreq>daily</changefreq>
-        <priority>0.9</priority>
-    </url>
-    <url>
-        <loc>{{ route('journal') }}</loc>
-        <changefreq>daily</changefreq>
-        <priority>0.9</priority>
-    </url>
+    @endforeach
 
     <!-- Dynamic Content -->
     @foreach ($hotels as $hotel)
+    @php
+        $slugTr = $hotel->slug_tr ?: $hotel->id;
+        $slugEn = $hotel->slug_en ?: $slugTr;
+    @endphp
     <url>
-        <loc>{{ route('otel.detay', $hotel->slug_tr ?: ($hotel->slug_en ?: $hotel->id)) }}</loc>
-        <lastmod>{{ $hotel->updated_at->tz('UTC')->toAtomString() }}</lastmod>
+        <loc>{{ route('otel.detay', $slugTr) }}</loc>
+        <xhtml:link rel="alternate" hreflang="tr" href="{{ route('otel.detay', ['slug_or_id' => $slugTr, 'lang' => 'tr']) }}"/>
+        <xhtml:link rel="alternate" hreflang="en" href="{{ route('otel.detay', ['slug_or_id' => $slugEn, 'lang' => 'en']) }}"/>
+        <xhtml:link rel="alternate" hreflang="x-default" href="{{ route('otel.detay', $slugTr) }}"/>
+        <lastmod>{{ $hotel->updated_at ? $hotel->updated_at->tz('UTC')->toAtomString() : date('c') }}</lastmod>
         <changefreq>weekly</changefreq>
         <priority>0.8</priority>
     </url>
     @endforeach
 
     @foreach ($restaurants as $restaurant)
+    @php
+        $slugTr = $restaurant->slug_tr ?: $restaurant->id;
+        $slugEn = $restaurant->slug_en ?: $slugTr;
+    @endphp
     <url>
-        <loc>{{ route('restoran.detay', $restaurant->slug_tr ?: ($restaurant->slug_en ?: $restaurant->id)) }}</loc>
-        <lastmod>{{ $restaurant->updated_at->tz('UTC')->toAtomString() }}</lastmod>
+        <loc>{{ route('restoran.detay', $slugTr) }}</loc>
+        <xhtml:link rel="alternate" hreflang="tr" href="{{ route('restoran.detay', ['slug_or_id' => $slugTr, 'lang' => 'tr']) }}"/>
+        <xhtml:link rel="alternate" hreflang="en" href="{{ route('restoran.detay', ['slug_or_id' => $slugEn, 'lang' => 'en']) }}"/>
+        <xhtml:link rel="alternate" hreflang="x-default" href="{{ route('restoran.detay', $slugTr) }}"/>
+        <lastmod>{{ $restaurant->updated_at ? $restaurant->updated_at->tz('UTC')->toAtomString() : date('c') }}</lastmod>
         <changefreq>weekly</changefreq>
         <priority>0.8</priority>
     </url>
     @endforeach
 
     @foreach ($yachts as $yacht)
+    @php
+        $slugTr = $yacht->slug_tr ?: $yacht->id;
+        $slugEn = $yacht->slug_en ?: $slugTr;
+    @endphp
     <url>
-        <loc>{{ route('yat.detay', $yacht->slug_tr ?: ($yacht->slug_en ?: $yacht->id)) }}</loc>
-        <lastmod>{{ $yacht->updated_at->tz('UTC')->toAtomString() }}</lastmod>
+        <loc>{{ route('yat.detay', $slugTr) }}</loc>
+        <xhtml:link rel="alternate" hreflang="tr" href="{{ route('yat.detay', ['slug_or_id' => $slugTr, 'lang' => 'tr']) }}"/>
+        <xhtml:link rel="alternate" hreflang="en" href="{{ route('yat.detay', ['slug_or_id' => $slugEn, 'lang' => 'en']) }}"/>
+        <xhtml:link rel="alternate" hreflang="x-default" href="{{ route('yat.detay', $slugTr) }}"/>
+        <lastmod>{{ $yacht->updated_at ? $yacht->updated_at->tz('UTC')->toAtomString() : date('c') }}</lastmod>
         <changefreq>weekly</changefreq>
         <priority>0.8</priority>
     </url>
     @endforeach
 
     @foreach ($destinations as $destination)
+    @php
+        $slugTr = $destination->slug_tr ?: $destination->id;
+        $slugEn = $destination->slug_en ?: $slugTr;
+    @endphp
     <url>
-        <loc>{{ route('destinasyon.detay', $destination->slug_tr ?: ($destination->slug_en ?: $destination->id)) }}</loc>
-        <lastmod>{{ $destination->updated_at->tz('UTC')->toAtomString() }}</lastmod>
+        <loc>{{ route('destinasyon.detay', $slugTr) }}</loc>
+        <xhtml:link rel="alternate" hreflang="tr" href="{{ route('destinasyon.detay', ['slug_or_id' => $slugTr, 'lang' => 'tr']) }}"/>
+        <xhtml:link rel="alternate" hreflang="en" href="{{ route('destinasyon.detay', ['slug_or_id' => $slugEn, 'lang' => 'en']) }}"/>
+        <xhtml:link rel="alternate" hreflang="x-default" href="{{ route('destinasyon.detay', $slugTr) }}"/>
+        <lastmod>{{ $destination->updated_at ? $destination->updated_at->tz('UTC')->toAtomString() : date('c') }}</lastmod>
         <changefreq>weekly</changefreq>
         <priority>0.8</priority>
     </url>
     @endforeach
     
     @foreach ($guides as $guide)
+    @php
+        $slugTr = $guide->slug_tr ?: $guide->id;
+        $slugEn = $guide->slug_en ?: $slugTr;
+    @endphp
     <url>
-        <loc>{{ route('rehber.detay', $guide->slug_tr ?: ($guide->slug_en ?: $guide->id)) }}</loc>
-        <lastmod>{{ $guide->updated_at->tz('UTC')->toAtomString() }}</lastmod>
+        <loc>{{ route('rehber.detay', $slugTr) }}</loc>
+        <xhtml:link rel="alternate" hreflang="tr" href="{{ route('rehber.detay', ['slug_or_id' => $slugTr, 'lang' => 'tr']) }}"/>
+        <xhtml:link rel="alternate" hreflang="en" href="{{ route('rehber.detay', ['slug_or_id' => $slugEn, 'lang' => 'en']) }}"/>
+        <xhtml:link rel="alternate" hreflang="x-default" href="{{ route('rehber.detay', $slugTr) }}"/>
+        <lastmod>{{ $guide->updated_at ? $guide->updated_at->tz('UTC')->toAtomString() : date('c') }}</lastmod>
         <changefreq>weekly</changefreq>
         <priority>0.8</priority>
     </url>
     @endforeach
 
     @foreach ($events as $event)
+    @php
+        $slugTr = $event->slug_tr ?: $event->id;
+        $slugEn = $event->slug_en ?: $slugTr;
+    @endphp
     <url>
-        <loc>{{ route('etkinlik.detay', $event->slug_tr ?: ($event->slug_en ?: $event->id)) }}</loc>
-        <lastmod>{{ $event->updated_at->tz('UTC')->toAtomString() }}</lastmod>
+        <loc>{{ route('etkinlik.detay', $slugTr) }}</loc>
+        <xhtml:link rel="alternate" hreflang="tr" href="{{ route('etkinlik.detay', ['slug_or_id' => $slugTr, 'lang' => 'tr']) }}"/>
+        <xhtml:link rel="alternate" hreflang="en" href="{{ route('etkinlik.detay', ['slug_or_id' => $slugEn, 'lang' => 'en']) }}"/>
+        <xhtml:link rel="alternate" hreflang="x-default" href="{{ route('etkinlik.detay', $slugTr) }}"/>
+        <lastmod>{{ $event->updated_at ? $event->updated_at->tz('UTC')->toAtomString() : date('c') }}</lastmod>
         <changefreq>weekly</changefreq>
         <priority>0.8</priority>
     </url>
     @endforeach
 
     @foreach ($journals as $journal)
+    @php
+        $slugTr = $journal->slug_tr ?: $journal->id;
+        $slugEn = $journal->slug_en ?: $slugTr;
+    @endphp
     <url>
-        <loc>{{ route('journal.detay', $journal->slug_tr ?: ($journal->slug_en ?: $journal->id)) }}</loc>
-        <lastmod>{{ $journal->updated_at->tz('UTC')->toAtomString() }}</lastmod>
+        <loc>{{ route('journal.detay', $slugTr) }}</loc>
+        <xhtml:link rel="alternate" hreflang="tr" href="{{ route('journal.detay', ['slug_or_id' => $slugTr, 'lang' => 'tr']) }}"/>
+        <xhtml:link rel="alternate" hreflang="en" href="{{ route('journal.detay', ['slug_or_id' => $slugEn, 'lang' => 'en']) }}"/>
+        <xhtml:link rel="alternate" hreflang="x-default" href="{{ route('journal.detay', $slugTr) }}"/>
+        <lastmod>{{ $journal->updated_at ? $journal->updated_at->tz('UTC')->toAtomString() : date('c') }}</lastmod>
         <changefreq>weekly</changefreq>
         <priority>0.8</priority>
     </url>
--- storage/framework/views/9f2c2376016f079dc66267f4ec6d5861.php
+++ storage/framework/views/9f2c2376016f079dc66267f4ec6d5861.php
@@ -1,107 +1,140 @@
 <?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+        xmlns:xhtml="http://www.w3.org/1999/xhtml">
     
     <!-- Static Pages -->
+    <?php
+        $staticRoutes = [
+            'home' => ['freq' => 'daily', 'prio' => '1.0'],
+            'hakkimizda' => ['freq' => 'weekly', 'prio' => '0.8'],
+            'oteller' => ['freq' => 'daily', 'prio' => '0.9'],
+            'yatlar' => ['freq' => 'daily', 'prio' => '0.9'],
+            'restoranlar' => ['freq' => 'daily', 'prio' => '0.9'],
+            'gezi-rehberi' => ['freq' => 'daily', 'prio' => '0.9'],
+            'etkinlikler' => ['freq' => 'daily', 'prio' => '0.9'],
+            'journal' => ['freq' => 'daily', 'prio' => '0.9'],
+        ];
+    ?>
+
+    <?php $__currentLoopData = $staticRoutes; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $routeName => $meta): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
     <url>
-        <loc><?php echo e(route('home')); ?></loc>
-        <changefreq>daily</changefreq>
-        <priority>1.0</priority>
+        <loc><?php echo e(route($routeName)); ?></loc>
+        <xhtml:link rel="alternate" hreflang="tr" href="<?php echo e(route($routeName, ['lang' => 'tr'])); ?>"/>
+        <xhtml:link rel="alternate" hreflang="en" href="<?php echo e(route($routeName, ['lang' => 'en'])); ?>"/>
+        <xhtml:link rel="alternate" hreflang="x-default" href="<?php echo e(route($routeName)); ?>"/>
+        <changefreq><?php echo e($meta['freq']); ?></changefreq>
+        <priority><?php echo e($meta['prio']); ?></priority>
     </url>
-    <url>
-        <loc><?php echo e(route('hakkimizda')); ?></loc>
-        <changefreq>weekly</changefreq>
-        <priority>0.8</priority>
-    </url>
-    <url>
-        <loc><?php echo e(route('oteller')); ?></loc>
-        <changefreq>daily</changefreq>
-        <priority>0.9</priority>
-    </url>
-    <url>
-        <loc><?php echo e(route('yatlar')); ?></loc>
-        <changefreq>daily</changefreq>
-        <priority>0.9</priority>
-    </url>
-    <url>
-        <loc><?php echo e(route('restoranlar')); ?></loc>
-        <changefreq>daily</changefreq>
-        <priority>0.9</priority>
-    </url>
-    <url>
-        <loc><?php echo e(route('gezi-rehberi')); ?></loc>
-        <changefreq>daily</changefreq>
-        <priority>0.9</priority>
-    </url>
-    <url>
-        <loc><?php echo e(route('etkinlikler')); ?></loc>
-        <changefreq>daily</changefreq>
-        <priority>0.9</priority>
-    </url>
-    <url>
-        <loc><?php echo e(route('journal')); ?></loc>
-        <changefreq>daily</changefreq>
-        <priority>0.9</priority>
-    </url>
+    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
 
     <!-- Dynamic Content -->
     <?php $__currentLoopData = $hotels; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $hotel): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+    <?php
+        $slugTr = $hotel->slug_tr ?: $hotel->id;
+        $slugEn = $hotel->slug_en ?: $slugTr;
+    ?>
     <url>
-        <loc><?php echo e(route('otel.detay', $hotel->slug_tr ?: ($hotel->slug_en ?: $hotel->id))); ?></loc>
-        <lastmod><?php echo e($hotel->updated_at->tz('UTC')->toAtomString()); ?></lastmod>
+        <loc><?php echo e(route('otel.detay', $slugTr)); ?></loc>
+        <xhtml:link rel="alternate" hreflang="tr" href="<?php echo e(route('otel.detay', ['slug_or_id' => $slugTr, 'lang' => 'tr'])); ?>"/>
+        <xhtml:link rel="alternate" hreflang="en" href="<?php echo e(route('otel.detay', ['slug_or_id' => $slugEn, 'lang' => 'en'])); ?>"/>
+        <xhtml:link rel="alternate" hreflang="x-default" href="<?php echo e(route('otel.detay', $slugTr)); ?>"/>
+        <lastmod><?php echo e($hotel->updated_at ? $hotel->updated_at->tz('UTC')->toAtomString() : date('c')); ?></lastmod>
         <changefreq>weekly</changefreq>
         <priority>0.8</priority>
     </url>
     <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
 
     <?php $__currentLoopData = $restaurants; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $restaurant): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+    <?php
+        $slugTr = $restaurant->slug_tr ?: $restaurant->id;
+        $slugEn = $restaurant->slug_en ?: $slugTr;
+    ?>
     <url>
-        <loc><?php echo e(route('restoran.detay', $restaurant->slug_tr ?: ($restaurant->slug_en ?: $restaurant->id))); ?></loc>
-        <lastmod><?php echo e($restaurant->updated_at->tz('UTC')->toAtomString()); ?></lastmod>
+        <loc><?php echo e(route('restoran.detay', $slugTr)); ?></loc>
+        <xhtml:link rel="alternate" hreflang="tr" href="<?php echo e(route('restoran.detay', ['slug_or_id' => $slugTr, 'lang' => 'tr'])); ?>"/>
+        <xhtml:link rel="alternate" hreflang="en" href="<?php echo e(route('restoran.detay', ['slug_or_id' => $slugEn, 'lang' => 'en'])); ?>"/>
+        <xhtml:link rel="alternate" hreflang="x-default" href="<?php echo e(route('restoran.detay', $slugTr)); ?>"/>
+        <lastmod><?php echo e($restaurant->updated_at ? $restaurant->updated_at->tz('UTC')->toAtomString() : date('c')); ?></lastmod>
         <changefreq>weekly</changefreq>
         <priority>0.8</priority>
     </url>
     <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
 
     <?php $__currentLoopData = $yachts; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $yacht): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+    <?php
+        $slugTr = $yacht->slug_tr ?: $yacht->id;
+        $slugEn = $yacht->slug_en ?: $slugTr;
+    ?>
     <url>
-        <loc><?php echo e(route('yat.detay', $yacht->slug_tr ?: ($yacht->slug_en ?: $yacht->id))); ?></loc>
-        <lastmod><?php echo e($yacht->updated_at->tz('UTC')->toAtomString()); ?></lastmod>
+        <loc><?php echo e(route('yat.detay', $slugTr)); ?></loc>
+        <xhtml:link rel="alternate" hreflang="tr" href="<?php echo e(route('yat.detay', ['slug_or_id' => $slugTr, 'lang' => 'tr'])); ?>"/>
+        <xhtml:link rel="alternate" hreflang="en" href="<?php echo e(route('yat.detay', ['slug_or_id' => $slugEn, 'lang' => 'en'])); ?>"/>
+        <xhtml:link rel="alternate" hreflang="x-default" href="<?php echo e(route('yat.detay', $slugTr)); ?>"/>
+        <lastmod><?php echo e($yacht->updated_at ? $yacht->updated_at->tz('UTC')->toAtomString() : date('c')); ?></lastmod>
         <changefreq>weekly</changefreq>
         <priority>0.8</priority>
     </url>
     <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
 
     <?php $__currentLoopData = $destinations; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $destination): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+    <?php
+        $slugTr = $destination->slug_tr ?: $destination->id;
+        $slugEn = $destination->slug_en ?: $slugTr;
+    ?>
     <url>
-        <loc><?php echo e(route('destinasyon.detay', $destination->slug_tr ?: ($destination->slug_en ?: $destination->id))); ?></loc>
-        <lastmod><?php echo e($destination->updated_at->tz('UTC')->toAtomString()); ?></lastmod>
+        <loc><?php echo e(route('destinasyon.detay', $slugTr)); ?></loc>
+        <xhtml:link rel="alternate" hreflang="tr" href="<?php echo e(route('destinasyon.detay', ['slug_or_id' => $slugTr, 'lang' => 'tr'])); ?>"/>
+        <xhtml:link rel="alternate" hreflang="en" href="<?php echo e(route('destinasyon.detay', ['slug_or_id' => $slugEn, 'lang' => 'en'])); ?>"/>
+        <xhtml:link rel="alternate" hreflang="x-default" href="<?php echo e(route('destinasyon.detay', $slugTr)); ?>"/>
+        <lastmod><?php echo e($destination->updated_at ? $destination->updated_at->tz('UTC')->toAtomString() : date('c')); ?></lastmod>
         <changefreq>weekly</changefreq>
         <priority>0.8</priority>
     </url>
     <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
     
     <?php $__currentLoopData = $guides; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $guide): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+    <?php
+        $slugTr = $guide->slug_tr ?: $guide->id;
+        $slugEn = $guide->slug_en ?: $slugTr;
+    ?>
     <url>
-        <loc><?php echo e(route('rehber.detay', $guide->slug_tr ?: ($guide->slug_en ?: $guide->id))); ?></loc>
-        <lastmod><?php echo e($guide->updated_at->tz('UTC')->toAtomString()); ?></lastmod>
+        <loc><?php echo e(route('rehber.detay', $slugTr)); ?></loc>
+        <xhtml:link rel="alternate" hreflang="tr" href="<?php echo e(route('rehber.detay', ['slug_or_id' => $slugTr, 'lang' => 'tr'])); ?>"/>
+        <xhtml:link rel="alternate" hreflang="en" href="<?php echo e(route('rehber.detay', ['slug_or_id' => $slugEn, 'lang' => 'en'])); ?>"/>
+        <xhtml:link rel="alternate" hreflang="x-default" href="<?php echo e(route('rehber.detay', $slugTr)); ?>"/>
+        <lastmod><?php echo e($guide->updated_at ? $guide->updated_at->tz('UTC')->toAtomString() : date('c')); ?></lastmod>
         <changefreq>weekly</changefreq>
         <priority>0.8</priority>
     </url>
     <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
 
     <?php $__currentLoopData = $events; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $event): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+    <?php
+        $slugTr = $event->slug_tr ?: $event->id;
+        $slugEn = $event->slug_en ?: $slugTr;
+    ?>
     <url>
-        <loc><?php echo e(route('etkinlik.detay', $event->slug_tr ?: ($event->slug_en ?: $event->id))); ?></loc>
-        <lastmod><?php echo e($event->updated_at->tz('UTC')->toAtomString()); ?></lastmod>
+        <loc><?php echo e(route('etkinlik.detay', $slugTr)); ?></loc>
+        <xhtml:link rel="alternate" hreflang="tr" href="<?php echo e(route('etkinlik.detay', ['slug_or_id' => $slugTr, 'lang' => 'tr'])); ?>"/>
+        <xhtml:link rel="alternate" hreflang="en" href="<?php echo e(route('etkinlik.detay', ['slug_or_id' => $slugEn, 'lang' => 'en'])); ?>"/>
+        <xhtml:link rel="alternate" hreflang="x-default" href="<?php echo e(route('etkinlik.detay', $slugTr)); ?>"/>
+        <lastmod><?php echo e($event->updated_at ? $event->updated_at->tz('UTC')->toAtomString() : date('c')); ?></lastmod>
         <changefreq>weekly</changefreq>
         <priority>0.8</priority>
     </url>
     <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
 
     <?php $__currentLoopData = $journals; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $journal): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+    <?php
+        $slugTr = $journal->slug_tr ?: $journal->id;
+        $slugEn = $journal->slug_en ?: $slugTr;
+    ?>
     <url>
-        <loc><?php echo e(route('journal.detay', $journal->slug_tr ?: ($journal->slug_en ?: $journal->id))); ?></loc>
-        <lastmod><?php echo e($journal->updated_at->tz('UTC')->toAtomString()); ?></lastmod>
+        <loc><?php echo e(route('journal.detay', $slugTr)); ?></loc>
+        <xhtml:link rel="alternate" hreflang="tr" href="<?php echo e(route('journal.detay', ['slug_or_id' => $slugTr, 'lang' => 'tr'])); ?>"/>
+        <xhtml:link rel="alternate" hreflang="en" href="<?php echo e(route('journal.detay', ['slug_or_id' => $slugEn, 'lang' => 'en'])); ?>"/>
+        <xhtml:link rel="alternate" hreflang="x-default" href="<?php echo e(route('journal.detay', $slugTr)); ?>"/>
+        <lastmod><?php echo e($journal->updated_at ? $journal->updated_at->tz('UTC')->toAtomString() : date('c')); ?></lastmod>
         <changefreq>weekly</changefreq>
         <priority>0.8</priority>
     </url>
